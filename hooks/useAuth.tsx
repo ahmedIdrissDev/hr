ff15a28c2bc9fceb27b0aede4176077525a9b465
AhmedIdrissDev <webocamw224@gmail.com>
2026-04-07 16:52:56 +0100

    fix and push

--- hooks/useAuth.tsx
+++ hooks/useAuth.tsx
@@ -1,13 +1,11 @@
 'use client'
 import React from 'react'
-import { SessionProvider } from "next-auth/react"
 import { ConvexProvider, ConvexReactClient } from 'convex/react'
 
 interface AuthProviderProps{
     children:React.ReactNode ,
 }
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
-
 const Provider = ({children }:AuthProviderProps) => {
   return (
 
